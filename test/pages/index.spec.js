73e2a80ee863ba88edb8be3da43696360f70f211
YusukeSato <yskprog022151@gmail.com>
2020-06-29 00:53:24 +0900

    isVueInstanceテスト追加

--- test/pages/index.spec.js
+++ test/pages/index.spec.js
@@ -4,6 +4,6 @@ import Index from "~/pages/index.vue";
 describe("Index.vue", () => {
   it("is a vue instance", () => {
     const wrapper = shallowMount(Index);
-    console.log(wrapper);
+    expect(wrapper.isVueInstance()).toBeTruthy();
   });
 });
