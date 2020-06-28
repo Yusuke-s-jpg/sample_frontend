a9a27f3e86af6cdd0c2d56044e16c528396c5c0f
YusukeSato <yskprog022151@gmail.com>
2020-06-28 22:05:59 +0900

    sass導入

--- components/common/HeaderNav.vue
+++ components/common/HeaderNav.vue
@@ -5,13 +5,29 @@
         <li>
           <nuxt-link to="/">タスク管理</nuxt-link>
         </li>
+        <li>
+          <button @click="show=true">
+            <p>モーダル表示</p>
+          </button>
+        </li>
       </ul>
     </nav>
+    <Modal @closeModal="show=false" v-if="show" />
   </div>
 </template>
 
 <script>
-export default {};
+import Modal from "~/components/common/Modal.vue";
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      show: false
+    };
+  }
+};
 </script>>
 
 <style scoped>
