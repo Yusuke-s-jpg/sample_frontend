230b734961a41c5370319b0631bfe87e46cc09d1
YusukeSato <yskprog022151@gmail.com>
2020-07-03 23:02:18 +0900

    新規追加移動

--- components/common/HeaderNav.vue
+++ components/common/HeaderNav.vue
@@ -5,42 +5,26 @@
         <li>
           <nuxt-link to="/">タスク管理</nuxt-link>
         </li>
-        <li>
-          <p @click="show = true" class="new">タスク作成</p>
-        </li>
+        <li></li>
       </ul>
     </nav>
-    <Modal @closeModal="show = false" v-if="show" title="タスク作成" />
   </div>
 </template>
 
 <script>
-import Modal from "~/components/common/Modal.vue";
-export default {
-  components: {
-    Modal
-  },
-  data() {
-    return {
-      show: false
-    };
-  }
-};
+export default {};
 </script>
 
 <style scoped>
 ul {
   display: flex;
 }
+
 li {
   list-style: none;
 }
+
 a {
   color: aliceblue;
-}
-.new {
-  margin-left: 15px;
-  color: aliceblue;
-  cursor: pointer;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="container">
-    <ArticleCards
-      v-for="article in articles"
-      :key="article.id"
-      class="article-card"
-      :article="article"
-    />
+    <div class="new-button">
+      <button @click="show = true">タスク作成</button>
+    </div>
+    <div class="article-wrapper">
+      <ArticleCards
+        v-for="article in articles"
+        :key="article.id"
+        class="article-card"
+        :article="article"
+      />
+    </div>
+    <Modal @closeModal="show = false" v-if="show" title="タスク作成" />
   </div>
 </template>
 
 <script>
 import ArticleCards from "~/components/article/ArticleCards.vue";
+import Modal from "~/components/common/Modal.vue";
 
 export default {
   components: {
-    ArticleCards
+    ArticleCards,
+    Modal
+  },
+  data() {
+    return {
+      show: false
+    };
   },
   async asyncData({ $axios }) {
     const res = await $axios.$get("http://localhost:10080/api/articles");
@@ -28,6 +41,16 @@ export default {
 
 <style>
 .container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 50px;
+}
+
+.article-wrapper {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
